b38dca60977ed5676669dd4b75e854a29dc43f74
root <adwulfran>
2018-05-21 09:01:28 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use('/static', express.static(__dirname + '/public'));
+app.use('/static/node_modules/web3/dist/web3.min.js', express.static(__dirname + '/node_modules/web3/dist/web3.min.js'));
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function(){
+    console.log("The frontend server is running on port 3000!");
+});
